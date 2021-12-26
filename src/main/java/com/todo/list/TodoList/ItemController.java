ff024e1200fb57528ffbe7673dd55ac91ac2c4c1
Evandro Terra Gonçalves <evandro18.goncalves@gmail.com>
2021-12-26 14:20:55 -0400

    fix: configure cors

--- src/main/java/com/todo/list/TodoList/ItemController.java
+++ src/main/java/com/todo/list/TodoList/ItemController.java
@@ -2,6 +2,7 @@ package com.todo.list.TodoList;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:5000", maxAge = 3600)
 @RestController
 public class ItemController {
   private final ItemRepository repository;
--- src/main/java/com/todo/list/TodoList/TodoListApplication.java
+++ src/main/java/com/todo/list/TodoList/TodoListApplication.java
@@ -2,15 +2,28 @@ package com.todo.list.TodoList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TodoListApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoListApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/").allowedOrigins("*");
+			}
+		};
 	}
 
 }
